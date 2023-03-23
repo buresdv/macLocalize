ac4392c95ed4a0607c018ed0307a867c47f349f1
David Bureš <buresdv@gmail.com>
2023-03-23 21:36:34 +0100

    Initial Commit

---
+++ macLocalize/macLocalizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  macLocalizeApp.swift
+//  macLocalize
+//
+//  Created by David Bureš on 23.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct macLocalizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
